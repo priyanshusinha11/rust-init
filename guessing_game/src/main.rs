ecb87c43e4dc3363ea7e65de82485623239b7b2f
Priyanshu Sinha <priyanshusinha.2807@gmail.com>
2026-02-07 15:46:48 +0530

    Ignore Cargo target dirs and stop tracking guessing_game/target

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,11 +1,26 @@
 use std::io;
 
+// fn main() {
+//     println!("guess the number!");
+//     println!("please input your guess.");
+//     let mut guess = String::new();
+//     io::stdin()
+//         .read_line(&mut guess)
+//         .expect("failed to read line");
+//     println!("you guessed: {guess}");
+// }
+
+use rand::Rng;
+
+
 fn main() {
     println!("guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("the secret number is: {secret_number}");
     println!("please input your guess.");
     let mut guess = String::new();
     io::stdin()
-    .read_line(&mut guess)
-    .expect("failed to read line");
+        .read_line(&mut guess)
+        .expect("failed to read line");
     println!("you guessed: {guess}");
 }
